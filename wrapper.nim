5a528cb1e6a18c1bd55721d3360357d53b14d9a6
EpicStuff <EpicStuff@users.noreply.github.com>
2024-08-23 18:31:14 -0400

    v0.1.2

--- wrapper.nim
+++ wrapper.nim
@@ -26,7 +26,7 @@
         - " = char
 ]#
 
-import os, strutils, std/strformat
+import os, strutils, std/strformat, macros
 
 # constants
 const indentation = "\t"
@@ -35,12 +35,14 @@ const indentation = "\t"
 var params = commandLineParams()
 
 # loop through command line parameters for first argument that doesn't start with `-`
-var file: string
-for num, param in params:
-    if param[0] != '-':
-        file = param
-        params.delete(num)
-        break
+# var file: string
+# for num, param in params:
+#     if param[0] != '-':
+#         file = param
+#         params.delete(num)
+#         break
+
+var file = "test.zxc"
 
 # load the code
 var code = readFile(file)
@@ -62,6 +64,11 @@ while true:
         break
     old_code = code
 
+
+var debug = 0
+proc test(i: int, v: string) = 
+    echo fmt"{i}, {v}"
+    debug += 1
 
 
 # deal with indentation
@@ -102,52 +109,64 @@ for num, line in pairs(lines):
         if not in_str:
             if char == '"':
                 while true:
+                    test(112, lines[num])
                     # if is "\""
                     try:
-                        if line[index..index+4] == "\"\\\"\"":
+                        if lines[num][index..index+4] == "\"\\\"\"":
                             # replace with '\''
-                            lines[num] = line[0..index-1] & "'\''" & line[index+5..^1]
+                            lines[num] = lines[num][0..index-1] & "'\''" & lines[num][index+5..^1]
                             break
                     except IndexDefect:
                         discard;
                     # if is """
                     try:
-                        if line[index..index+3] == "\"\"\"":
-                            echo line
+                        if lines[num][index..index+3] == "\"\"\"":
+                            test(124, lines[num])
                             # replace """ with '''
-                            lines[num] = line[0..index-1] & "'\''" & line[index+3..^1]
+                            lines[num] = lines[num][0..index-1] & "'\''" & lines[num][index+3..^1]
                             break
                     except IndexDefect:
                         discard;
                     # if is ""
                     try:
-                        if line[index..index+2] == "\"\"":
+                        if lines[num][index..index+2] == "\"\"":
                             raise newException(ValueError,
-                                    fmt"empty char detected (and is not allowed), line: {num + 1}, char: {index + 1}")
+                                    fmt"empty char detected (and is not allowed), lines[num]: {num + 1}, char: {index + 1}")
                     except IndexDefect:
                         discard;
                         # replace " with '
-                        lines[num] = line[0..index-1] & "'" & line[index+1..^1]
+                        lines[num] = lines[num][0..index-1] & "'" & lines[num][index+1..^1]
                         break
 
         # dealing with strings
+            test(142, lines[num])
             if char == '\'':
-                lines[num] = line[0..index-1] & "\"" & line[index+1..^1]
+                echo char
+                echo lines[num][0..index-2]
+                lines[num] = lines[num][0..index-2] & "\"" & lines[num][index..^1]
+                test(145, lines[num])
                 in_str = true
         else:
+            test(148, lines[num])
             # if " in a string, escape it
             if char == '"':
-                lines[num] = line[0..index-1] & '\"' & line[index+1..^1]
-            if char == '\'' and line[index-1] != '\\':
-                lines[num] = line[0..index-1] & '\"' & line[index+1..^1]
+                lines[num] = lines[num][0..index-1] & '\"' & lines[num][index+1..^1]
+            if char == '\'' and lines[num][index-1] != '\\':
+                try:
+                    lines[num] = lines[num][0..index-2] & '\"' & lines[num][index..^1]
+                except RangeDefect:
+                    lines[num] = lines[num][0..index-2] & '\"'
                 in_str = false
+        test(155, lines[num])
 
     ## deal with indentation
-    lines[num] = repeat("  ", indent) & line
-    if line[^1] == ':':
+    test(158, lines[num])
+    lines[num] = repeat("  ", indent) & lines[num]
+    if lines[num][^1] == ':':
         indent += 1
-    elif line[^1] == ';':
+    elif lines[num][^1] == ';':
         indent -= 1
 
 # save the code
-writeFile(file.replace(".zxc", ".nim"), code.join("\n"))
+echo lines.join("\n")
+writeFile(file.replace(".zxc", ".nim"), lines.join("\n"))
