a487dff8433d94bc49cc0fb5142d356e96b8885f
EpicStuff <EpicStuff@users.noreply.github.com>
2024-08-23 20:24:37 -0400

    v0.2.1
    nvm, not actually using strslice

--- wrapper.nim
+++ wrapper.nim
@@ -26,7 +26,7 @@
         - " = char
 ]#
 
-import os, strutils, std/strformat, strslice
+import os, strutils, std/strformat, strslice, std/sequtils
 
 # constants
 const indentation = "\t"
@@ -35,26 +35,17 @@ const indentation = "\t"
 var params = commandLineParams()
 
 # loop through command line parameters for first argument that doesn't start with `-`
-# var file: string
-# for num, param in params:
-#     if param[0] != '-':
-#         file = param
-#         params.delete(num)
-#         break
+var file: string
+for num, param in params:
+    if param[0] != '-':
+        file = param
+        params.delete(num)
+        break
 
-var file = "test.zxc"
+# var file = "test.zxc"
 
 # load the code
 var code = readFile(file)
-
-# # find unused placeholder
-# var placeholder: string
-# const placeholders = ['“', '”']
-# for placeholder in placeholders:
-#     if placeholder not in code:
-#         break
-# else:
-#     raise newException(ValueError, 'No unused placeholder found')
 
 # remove trailing whitespace
 var old_code = code
@@ -65,12 +56,6 @@ while true:
     old_code = code
 
 
-var debug = 0
-proc test(i: int, v: string) = 
-    echo fmt"{i}, {v}"
-    debug += 1
-
-
 # deal with indentation
 var indent = 0
 var comment = 0
@@ -78,16 +63,16 @@ var in_str = false
 
 var lines = splitLines(code)
 for num, lIne in pairs(lines):
-    var line = lIne
+    var line = strip(lIne).toSeq()
     # skip empty lines
-    if line == "":
+    if line == @[]:
         continue
     ## deal with comments
     # skip comments (if line, ignoring indentation starts with #)
-    if line.replace(indentation, "")[0] == '#':
+    if line[0] == '#':
         continue
     # check if is start of multiline comment (if line, ignoring indentation starts with #[)
-    if line.replace(indentation, "")[0..1] == "#[":
+    if line[0..1] == "#[":
         comment += 1
     # check if is end of multiline comment
     if line[^2..^1] == "#]" and comment > 0:
@@ -100,8 +85,6 @@ for num, lIne in pairs(lines):
     # to do
 
     ## if not in a comment/string block
-    # get rid of indentation
-    line = strip(line)
     # go character by character
     for index, char in pairs(line):
         ## swap quotes
@@ -110,7 +93,6 @@ for num, lIne in pairs(lines):
         if not in_str:
             if char == '"':
                 while true:
-                    test(112, line)
                     # if is "\""
                     try:
                         if line[index..index+4] == "\"\\\"\"" or line[index..index+3] == "\"\"\"":
@@ -125,40 +107,40 @@ for num, lIne in pairs(lines):
                             raise newException(ValueError, fmt"empty char detected (and is not allowed), lines[num]: {num + 1}, char: {index + 1}")
                     except IndexDefect:
                         discard;
-                        # replace " with '
-                        line[index] = '\''
-                        break
+                    # replace " with '
+                    line[index] = '\''
+                    break
 
         # dealing with strings
-            test(142, line)
+            
             if char == '\'':
-                echo char
-                echo lines[num][0..index-2]
+                
+                
                 line[index] = '\"'
-                test(145, line)
+                
                 in_str = true
         else:
-            test(148, line)
+            
             # if " in a string, escape it
             if char == '"':
                 line[index..index] = "\\\""
             if char == '\'' and line[index-1] != '\\':
                 line[index] = '\"'
                 in_str = false
-        test(155, line)
+        
 
     ## deal with indentation
-    test(158, line)
-    echo indent
-    line = repeat("  ", indent) & line
-    echo line
+    
+    
+    line[0..0] = repeat("  ", indent) & line[0]
+    
     if line[^1] == ':':
         indent += 1
     elif line[^1] == ';':
         indent -= 1
 
-    lines[num] = line
+    lines[num] = line.join()
 
 # save the code
-echo lines.join("\n")
+
 writeFile(file.replace(".zxc", ".nim"), lines.join("\n"))
